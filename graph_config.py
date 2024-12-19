efcb2b8a70c173ae507a7f35074920c988306f1e
OTovy <40425375+OTovy@users.noreply.github.com>
2024-12-20 01:16:56 +0200

    Update graph_config.py

--- graph_config.py
+++ graph_config.py
@@ -1,7 +1,6 @@
 
 node_dict = [
     {'Node': 'Shaolin', 'linked_nodes': ['Southern Shaolin', 'Five Elders']}
-    , {'Node': 'Five Elders', 'linked_nodes': ['Five Elder Families']}
     , {'Node': 'Five Elders', 'linked_nodes': ['Miu Hin', 'Fung Dou Dak', 'Ng Mui', 'Ji Sin', 'Bak Mei']}
     , {'Node': 'Southern Shaolin', 'linked_nodes': []}
     , {'Node': 'Bak Mei', 'linked_nodes': ['Bak Mei Pai']}
