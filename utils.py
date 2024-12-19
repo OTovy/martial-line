8c6b79fef703c76973c628766571d4bedb1934ef
OTovy <40425375+OTovy@users.noreply.github.com>
2024-12-20 00:26:07 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,20 @@
 
 import graphviz
 
-def create_graphviz_chart(nodes, edges):
+def create_graphviz_chart(nodes_dict):
   """
-  Creates a Graphviz chart with nodes and edges.
+  Creates a Graphviz chart from a dictionary of nodes and their linked nodes.
 
   Args:
-    nodes: A list of node names.
-    edges: A list of tuples, where each tuple represents an edge 
-           between two nodes (e.g., ('A', 'B')).
+    node_dict: A list of dictionaries, where each dictionary 
+               represents a node and its linked nodes.
+               Example: 
+               [
+                 {'Node': 'A', 'linked_nodes': ['B', 'C']}, 
+                 {'Node': 'B', 'linked_nodes': ['D']}, 
+                 {'Node': 'C', 'linked_nodes': []}, 
+                 {'Node': 'D', 'linked_nodes': []}
+               ]
 
   Returns:
     A Graphviz graph object.
@@ -16,12 +22,11 @@ def create_graphviz_chart(nodes, edges):
 
   graph = graphviz.Digraph()
 
-  # Add nodes to the graph
-  for node in nodes:
-    graph.node(node)
+  for node_data in nodes_dict:
+    node_name = node_data['Node']
+    graph.node(node_name)
 
-  # Add edges to the graph
-  for edge in edges:
-    graph.edge(*edge)
+    for linked_node in node_data['linked_nodes']:
+      graph.edge(node_name, linked_node)
 
   return graph
