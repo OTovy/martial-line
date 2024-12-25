6fb87ed4c40bf3cd74cde55d30e8db57d0aaca04
OTovy <40425375+OTovy@users.noreply.github.com>
2024-12-25 11:12:56 +0200

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,18 +1,26 @@
 
 var nodes_list = [
-{'id': 'Shaolin', 'label': 'Shaolin', 'linked_nodes': ['Southern Shaolin', 'Five Elders']},
+ {'id': 'Shaolin', 'label': 'Shaolin', 'linked_nodes': ['Southern Shaolin', 'Five Elders']},
  {'id': 'Southern Shaolin', 'label': 'Southern Shaolin', 'linked_nodes': ['Luo Han Quan (Monk Fist)']},
  {'id': 'Five Elders', 'label': 'Five Elders', 'linked_nodes': ['Miu Hin', 'Fung Dou Dak', 'Ng Mui', 'Ji Sin', 'Bak Mei']},
  {'id': 'Miu Hin', 'label': 'Miu Hin', 'linked_nodes': ['Yim Wing Chun']},
+ {'id': 'Fung Dou Dak', 'label': 'Fung Dou Dak', 'linked_nodes': []},
+ {'id': 'Ng Mui', 'label': 'Ng Mui', 'linked_nodes': []},
  {'id': 'Ji Sin', 'label': 'Ji Sin', 'linked_nodes': ['Five Elder Families']},
  {'id': 'Bak Mei', 'label': 'Bak Mei', 'linked_nodes': ['Bak Mei Pai']},
+ {'id': 'Bak Mei Pai', 'label': 'Bak Mei Pai', 'linked_nodes': []},
  {'id': 'Five Elder Families', 'label': 'Five Elder Families', 'linked_nodes': ['Lau Saam Ngan', 'Mok Ching Giu', 'Hung Hei-gun', 'Choy Gau Yi', 'Lei Yau Saan']},
  {'id': 'Hakka', 'label': 'Hakka', 'linked_nodes': ['Bak Mei Pai', 'Southern Praying Mantis', 'Southern Dragon', 'Phoenix Eye Fist (Chuka Shaolin)']},
+ {'id': 'Southern Praying Mantis', 'label': 'Southern Praying Mantis', 'linked_nodes': []},
+ {'id': 'Southern Dragon', 'label': 'Southern Dragon', 'linked_nodes': []},
+ {'id': 'Phoenix Eye Fist (Chuka Shaolin)', 'label': 'Phoenix Eye Fist (Chuka Shaolin)', 'linked_nodes': []},
  {'id': 'Lau Saam Ngan', 'label': 'Lau Saam Ngan', 'linked_nodes': ['Lau Ga']},
  {'id': 'Mok Ching Giu', 'label': 'Mok Ching Giu', 'linked_nodes': ['Mok Ga']},
  {'id': 'Hung Hei-gun', 'label': 'Hung Hei-gun', 'linked_nodes': ['Hung Ga']},
  {'id': 'Choy Gau Yi', 'label': 'Choy Gau Yi', 'linked_nodes': ['Choy Ga']},
  {'id': 'Lei Yau Saan', 'label': 'Lei Yau Saan', 'linked_nodes': ['Lei Ga']},
+ {'id': 'Lau Ga', 'label': 'Lau Ga', 'linked_nodes': []},
+ {'id': 'Mok Ga', 'label': 'Mok Ga', 'linked_nodes': []},
  {'id': 'Hung Ga', 'label': 'Hung Ga', 'linked_nodes': ['Fut Ga Buddha Family']},
  {'id': 'Choy Ga', 'label': 'Choy Ga', 'linked_nodes': ['Fut Ga Buddha Family', 'Chan Heung']},
  {'id': 'Lei Ga', 'label': 'Lei Ga', 'linked_nodes': ['Chan Heung']},
@@ -21,9 +29,20 @@ var nodes_list = [
  {'id': 'Yim Wing Chun', 'label': 'Yim Wing Chun', 'linked_nodes': ['Leung Bok-chao', 'Leung Jan', 'Leung Yee-tai']},
  {'id': 'Leung Bok-chao', 'label': 'Leung Bok-chao', 'linked_nodes': ['Leung Lan-kwai', 'Wong Wah-bo']},
  {'id': 'Leung Jan', 'label': 'Leung Jan', 'linked_nodes': ['Chan Wan-shun', 'Ip Man']},
+ {'id': 'Leung Yee-tai', 'label': 'Leung Yee-tai', 'linked_nodes': []},
+ {'id': 'Leung Lan-kwai', 'label': 'Leung Lan-kwai', 'linked_nodes': []},
+ {'id': 'Wong Wah-bo', 'label': 'Wong Wah-bo', 'linked_nodes': []},
+ {'id': 'Chan Wan-shun', 'label': 'Chan Wan-shun', 'linked_nodes': []},
  {'id': 'Ip Man', 'label': 'Ip Man', 'linked_nodes': ['Bruce Lee', 'Wu Wei']},
  {'id': 'Bruce Lee', 'label': 'Bruce Lee', 'linked_nodes': ['Jeet Kune Do', 'Joseph Cowles']},
- {'id': 'Luo Han Quan (Monk Fist)', 'label': 'Luo Han Quan (Monk Fist)', 'linked_nodes': ['Incense Shop Boxing', 'Fang Zhengdong']}
-];
+ {'id': 'Wu Wei', 'label': 'Wu Wei', 'linked_nodes': []},
+ {'id': 'Jeet Kune Do', 'label': 'Jeet Kune Do', 'linked_nodes': []},
+ {'id': 'Joseph Cowles', 'label': 'Joseph Cowles', 'linked_nodes': []},
+ {'id': 'Luo Han Quan (Monk Fist)', 'label': 'Luo Han Quan (Monk Fist)', 'linked_nodes': ['Incense Shop Boxing', 'Fang Zhengdong']},
+ {'id': 'Incense Shop Boxing', 'label': 'Incense Shop Boxing', 'linked_nodes': []},
+ {'id': 'Fang Zhengdong', 'label': 'Fang Zhengdong', 'linked_nodes': []},
+ {'id': 'Choy Lei Fut', 'label': 'Choy Lei Fut', 'linked_nodes': []}
+ ];
+
 
 export { nodes_list };  
