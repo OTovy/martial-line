daf58da788d6506b049adf6b77953ed6630ffe37
OTovy <40425375+OTovy@users.noreply.github.com>
2024-12-29 22:37:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,9 @@ style: [
       'line-color': '#000000',
       'target-arrow-color': '#000000',
       'target-arrow-shape': 'triangle',
-      'z-index': 1
+      'z-index': 1,
+      'control-point-distance': 50, // Optional: Add control points
+      'control-point-weight': 0.5   // Optional: Add control weight
 
     }
   }
